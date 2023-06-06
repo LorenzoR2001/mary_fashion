d62f7e04e146e1f8e36ba8944162b666a5acc945
Lorenzo Romano <Lor.Romano@multiversity.local>
2023-06-06 15:20:20 +0200

    fix

--- src/components/Me.vue
+++ src/components/Me.vue
@@ -1,79 +1,73 @@
 <template>
-    <div class="container bg-primary  mx-auto  p-16 lg:px-32  ">
-        <div class="w-full grid lg:grid-cols-2">
-            <div> <img class="lg:w-[75%] w-100" src="../assets/mary2.jpg" /></div>
-            <div>
-                <div class="w-full    ">
-                    <div class="flex justify-center ">
-                        <h2 class="text-mywhite font-bold font-mytext text-4xl lg:mt-0 mt-8">Mi presento</h2>
-                    </div>
-                    <div class="mt-8">
-                        <div class="w-full grid gap-y-4 lg:grid-cols-2">
-                            <div class=" flex justify-center lg:block">
+  <div class="container bg-primary mx-auto p-16 lg:px-32">
+    <div class="w-full grid lg:grid-cols-2">
+      <div><img class="lg:w-[75%] w-100" src="../assets/mary2.jpg" /></div>
+      <div>
+        <div class="w-full">
+          <div class="flex justify-center">
+            <h2
+              class="text-mywhite font-bold font-mytext text-4xl lg:mt-0 mt-8"
+            >
+              Mi presento
+            </h2>
+          </div>
+          <div class="mt-8">
+            <div class="w-full grid gap-y-4 lg:grid-cols-4">
+              <div class="flex lg:col-span-1 justify-center lg:block">
+                <p class="text-myblack text-2xl">Nome</p>
+              </div>
+              <div class="flex lg:col-span-3 lg:justify-end justify-center">
+                <span class="text-2xl text-mywhite font-bold"
+                  >Maria Federico</span
+                >
+              </div>
+              <div class="flex  lg:col-span-1 justify-center lg:block">
+                <p class="text-myblack text-2xl">Età</p>
+              </div>
+              <div class="flex lg:col-span-3 lg:justify-end justify-center">
+                <span class="text-2xl text-mywhite font-bold"
+                  >{{ calculate_age(new Date(2002, 10, 20)) }}
+                </span>
+              </div>
 
-                                <p class="text-myblack text-2xl">Nome
-                                </p>
-                            </div>
-                            <div class="flex lg:justify-end justify-center">
-
-                                <span class="text-2xl text-mywhite font-bold">Maria
-                                    Federico</span>
-
-                            </div>
-                            <div class=" flex justify-center lg:block">
-                                <p class="text-myblack text-2xl">Età </p>
-                            </div>
-                            <div class="flex lg:justify-end justify-center">
-                                <span class="text-2xl text-mywhite font-bold">{{ calculate_age(new Date(2002, 10, 20)) }}
-                                </span>
-
-                            </div>
-
-                            <div class=" flex justify-center lg:block">
-                                <p class="text-myblack text-2xl">Città </p>
-                            </div>
-                            <div class="flex lg:justify-end justify-center">
-
-                                <span class="text-2xl text-mywhite font-bold">Casalnuovo (Na)
-                                </span>
-                            </div>
-                            <div class=" flex justify-center lg:block">
-                                <p class="text-myblack text-2xl">Cellulare </p>
-                            </div>
-                            <div class="flex lg:justify-end justify-center">
-
-                                <span class="text-2xl text-mywhite font-bold underline"> <a href="tel:+39 366 420 5971">+39
-                                        366 420 5971</a>
-
-                                </span>
-                            </div>
-                            <div class=" flex justify-center lg:block">
-
-                                <p class="text-myblack text-2xl">Orari </p>
-                            </div>
-                            <div class="flex lg:justify-end justify-center">
-
-                                <span class="text-2xl text-mywhite font-bold  ">Lun-Sab (07:00-19:00)
-
-                                </span>
-                            </div>
-                        </div>
-
-
-
-
-                    </div>
-                </div>
+              <div class="flex lg:col-span-1 justify-center lg:block">
+                <p class="text-myblack text-2xl">Città</p>
+              </div>
+              <div class="flex lg:col-span-3 lg:justify-end justify-center">
+                <span class="text-2xl text-mywhite font-bold"
+                  >Casalnuovo (Na)
+                </span>
+              </div>
+              <div class="flex lg:col-span-1 justify-center lg:block">
+                <p class="text-myblack text-2xl">Cellulare</p>
+              </div>
+              <div class="flex lg:col-span-3 lg:justify-end justify-center">
+                <span
+                  class="text-2xl  text-mywhite font-bold underline"
+                >
+                  <a href="tel:+39 366 420 5971">+39 366 420 5971</a>
+                </span>
+              </div>
+              <div class="lg:col-span-1 flex justify-center lg:block">
+                <p class="text-myblack text-2xl">Orari</p>
+              </div>
+              <div class="flex lg:col-span-3 lg:justify-end justify-center">
+                <span class="text-2xl text-mywhite font-bold"
+                  >Lun-Sab (07:00-19:00)
+                </span>
+              </div>
             </div>
-
+          </div>
         </div>
+      </div>
     </div>
+  </div>
 </template>
 <script setup>
 function calculate_age(dob) {
-    var diff_ms = Date.now() - dob.getTime();
-    var age_dt = new Date(diff_ms);
+  var diff_ms = Date.now() - dob.getTime();
+  var age_dt = new Date(diff_ms);
 
-    return Math.abs(age_dt.getUTCFullYear() - 1970);
+  return Math.abs(age_dt.getUTCFullYear() - 1970);
 }
 </script>
